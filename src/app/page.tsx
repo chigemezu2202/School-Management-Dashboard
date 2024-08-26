29653c44d875c2dffd94b06db3a56c95a79d62d3
chigemezu2202 <chigemezuezimoha@gmail.comgit config --global user.name chigemezu2202git config --global user.email chigemezuezimoha@gmail.com>
2024-08-26 19:33:05 +1300

    started testing out the bootstraped project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
+    <div className="">
+      Testing out project in progress &quot; School Management System &quot;
+    </div>
+  );
 }
 
 export default Homepage
